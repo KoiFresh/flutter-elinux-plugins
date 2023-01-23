e811107266d6263d5d6bb964a9f7e6019e84b26f
Kai Mayer <kai.mayer@t-online.de>
2023-01-23 21:38:30 +0000

    [audioplayers_elinux] feat: add methods to handle from audioplayers plugin

--- packages/audioplayers_elinux/elinux/audioplayers_elinux_plugin.cc
+++ packages/audioplayers_elinux/elinux/audioplayers_elinux_plugin.cc
@@ -4,66 +4,115 @@
 #include <flutter/plugin_registrar.h>
 #include <flutter/standard_method_codec.h>
 
+#include <variant>
+
 #include <map>
 #include <memory>
 #include <sstream>
 
+#include <iostream>
+#include <string>
+
 namespace {
 
-class AudioplayersElinuxPlugin : public flutter::Plugin {
- public:
-  static void RegisterWithRegistrar(flutter::PluginRegistrar *registrar);
+    constexpr char kAudioplayersChannelName[] = "xyz.luan/audioplayers";
+    constexpr char kAudioplayersChannelApiPause[] = "pause";
+    constexpr char kAudioplayersChannelApiResume[] = "resume";
+    constexpr char kAudioplayersChannelApiStop[] = "stop";
+    constexpr char kAudioplayersChannelApiRelease[] = "release";
+    constexpr char kAudioplayersChannelApiSeek[] = "seek";
+    constexpr char kAudioplayersChannelApiSetSourceUrl[] = "setSourceUrl";
+    constexpr char kAudioplayersChannelApiGetDuration[] = "getDuration";
+    constexpr char kAudioplayersChannelApiSetVolume[] = "setVolume";
+    constexpr char kAudioplayersChannelApiGetCurrentPosition[] = "getCurrentPosition";
+    constexpr char kAudioplayersChannelApiSetPlaybackRate[] = "setPlaybackRate";
+    constexpr char kAudioplayersChannelApiSetReleaseMode[] = "setReleaseMode";
+    constexpr char kAudioplayersChannelApiSetPlayerMode[] = "setPlayerMode";
+    constexpr char kAudioplayersChannelApiSetBalance[] = "setBalance";
 
-  AudioplayersElinuxPlugin();
+    class AudioplayersElinuxPlugin : public flutter::Plugin {
+    public:
+        static void RegisterWithRegistrar(flutter::PluginRegistrar *registrar);
 
-  virtual ~AudioplayersElinuxPlugin();
+        AudioplayersElinuxPlugin();
 
- private:
-  // Called when a method is called on this plugin's channel from Dart.
-  void HandleMethodCall(
-      const flutter::MethodCall<flutter::EncodableValue> &method_call,
-      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
-};
+        virtual ~AudioplayersElinuxPlugin();
 
-// static
-void AudioplayersElinuxPlugin::RegisterWithRegistrar(
-    flutter::PluginRegistrar *registrar) {
-  auto channel =
-      std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
-          registrar->messenger(), "audioplayers_elinux",
-          &flutter::StandardMethodCodec::GetInstance());
+    private:
+        // Called when a method is called on this plugin's channel from Dart.
+        void HandleMethodCall(const flutter::MethodCall<flutter::EncodableValue> &method_call, std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
 
-  auto plugin = std::make_unique<AudioplayersElinuxPlugin>();
+        void HandleSetSourceUrlCall(const flutter::EncodableValue* message, std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+    };
 
-  channel->SetMethodCallHandler(
-      [plugin_pointer = plugin.get()](const auto &call, auto result) {
-        plugin_pointer->HandleMethodCall(call, std::move(result));
-      });
+    // static
+    void AudioplayersElinuxPlugin::RegisterWithRegistrar(flutter::PluginRegistrar *registrar) {
+        auto channel = std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(registrar->messenger(), kAudioplayersChannelName, &flutter::StandardMethodCodec::GetInstance());
 
-  registrar->AddPlugin(std::move(plugin));
-}
+        auto plugin = std::make_unique<AudioplayersElinuxPlugin>();
 
-AudioplayersElinuxPlugin::AudioplayersElinuxPlugin() {}
+        channel->SetMethodCallHandler([plugin_pointer = plugin.get()](const auto &call, auto result) {
+            plugin_pointer->HandleMethodCall(call, std::move(result));
+        });
 
-AudioplayersElinuxPlugin::~AudioplayersElinuxPlugin() {}
+        registrar->AddPlugin(std::move(plugin));
+    }
 
-void AudioplayersElinuxPlugin::HandleMethodCall(
-    const flutter::MethodCall<flutter::EncodableValue> &method_call,
-    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-  if (method_call.method_name().compare("getPlatformVersion") == 0) {
-    std::ostringstream version_stream;
-    version_stream << "eLinux";
-    result->Success(flutter::EncodableValue(version_stream.str()));
-  } else {
-    result->NotImplemented();
-  }
-}
+    AudioplayersElinuxPlugin::AudioplayersElinuxPlugin() {}
 
+    AudioplayersElinuxPlugin::~AudioplayersElinuxPlugin() {}
+
+    void AudioplayersElinuxPlugin::HandleMethodCall(const flutter::MethodCall<flutter::EncodableValue> &method_call, std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
+
+        if (method_call.method_name().compare(kAudioplayersChannelApiPause) == 0) {
+            result->NotImplemented();
+
+        } else if (method_call.method_name().compare(kAudioplayersChannelApiResume) == 0) {
+            result->NotImplemented();
+
+        } else if (method_call.method_name().compare(kAudioplayersChannelApiStop) == 0) {
+            result->NotImplemented();
+
+        } else if (method_call.method_name().compare(kAudioplayersChannelApiRelease) == 0) {
+            result->NotImplemented();
+
+        } else if (method_call.method_name().compare(kAudioplayersChannelApiSeek) == 0) {
+            result->NotImplemented();
+
+        } else if (method_call.method_name().compare(kAudioplayersChannelApiSetSourceUrl) == 0) {
+            HandleSetSourceUrlCall(method_call.arguments(), std::move(result));
+
+        } else if (method_call.method_name().compare(kAudioplayersChannelApiGetDuration) == 0) {
+            result->NotImplemented();
+        
+        } else if (method_call.method_name().compare(kAudioplayersChannelApiSetVolume) == 0) {
+            result->NotImplemented();
+        
+        } else if (method_call.method_name().compare(kAudioplayersChannelApiGetCurrentPosition) == 0) {
+            result->NotImplemented();
+
+        } else if (method_call.method_name().compare(kAudioplayersChannelApiSetPlaybackRate) == 0) {
+            result->NotImplemented();
+
+        } else if (method_call.method_name().compare(kAudioplayersChannelApiSetReleaseMode) == 0) {
+            result->NotImplemented();
+
+        } else if (method_call.method_name().compare(kAudioplayersChannelApiSetPlayerMode) == 0) {
+            result->NotImplemented();
+
+        } else if (method_call.method_name().compare(kAudioplayersChannelApiSetBalance) == 0) {
+            result->NotImplemented();
+
+        } else  {
+            result->NotImplemented();
+        }
+    } // HandleMethodCall
 }  // namespace
 
-void AudioplayersElinuxPluginRegisterWithRegistrar(
-    FlutterDesktopPluginRegistrarRef registrar) {
-  AudioplayersElinuxPlugin::RegisterWithRegistrar(
-      flutter::PluginRegistrarManager::GetInstance()
-          ->GetRegistrar<flutter::PluginRegistrar>(registrar));
+void AudioplayersElinuxPlugin::HandleSetSourceUrlCall(const flutter::EncodableValue* message, std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
+    result->NotImplemented();
+}
+
+void AudioplayersElinuxPluginRegisterWithRegistrar(FlutterDesktopPluginRegistrarRef registrar) {
+  AudioplayersElinuxPlugin::RegisterWithRegistrar(flutter::PluginRegistrarManager::GetInstance()->GetRegistrar<flutter::PluginRegistrar>(registrar));
 }
