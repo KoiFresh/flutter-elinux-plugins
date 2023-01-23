93e03c658cd6e5ab98c6e495b2690dbefc0a10ea
Kai Mayer <kai.mayer@t-online.de>
2023-01-23 16:03:07 +0000

    [audioplayers_elinux] feat: add Logger from audioplayers_linux https://github.com/bluefireteam/audioplayers/blob/main/packages/audioplayers_linux/linux/Logger.h

---
+++ packages/audioplayers_elinux/elinux/Logger.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+enum class LogLevel { None, Error, Info };
+
+class Logger {
+ private:
+  static void Log(LogLevel level, std::string message);
+
+ public:
+  static LogLevel logLevel;
+
+  static void Info(std::string message);
+
+  static void Error(std::string message);
+};
